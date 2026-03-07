6d55a95920812975452cef0719186934d3d1db17
ayosleepy <filippmello2007@gmail.com>
2026-03-07 09:22:30 +0700

    kanban card component

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+Vue.component('kanban-card', {
+    props: ['card'],
+    template: `
+        <div class="card">
+            <div class="card-header">
+                <h3>{{ card.title }}</h3>
+                <div class="card-actions">
+                    <button @click="$emit('edit-card', card)">E</button>
+                    <button v-if="card.columnId === 1" @click="$emit('delete-card', card)">X</button>
+                </div>
+            </div>
+            <p>{{ card.description }}</p>
+            <div class="date">Created: {{ card.createdAt }}</div>
+            <div class="date">Deadline: {{ card.deadline }}</div>
+            <div v-if="card.lastEdited" class="date">Edited: {{ card.lastEdited }}</div>
+        </div>
+    `,
+})
+
 let app = new Vue({
     el: '#app',
     data: {
